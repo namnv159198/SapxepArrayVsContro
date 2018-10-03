85906d7fa5939f48e5c25a4161ce9f86a2dab3f7
namnvth1809002 <namnvth1809002@fpt.edu.vn>
2018-10-03 20:24:34 +0700

    Ung dung Sap xep mang bang con tro

---
+++ cmake-build-debug/CMakeFiles/MangVs_Contro.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MangVs_Contro.dir/main.c.obj"
+  "MangVs_Contro.pdb"
+  "MangVs_Contro.exe"
+  "MangVs_Contro.exe.manifest"
+  "libMangVs_Contro.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MangVs_Contro.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+void Hoanvi(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void xuat(int a[], int n) {
+    for (int i = 0; i < n; ++i) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+}
+
+void sapxeptangdan(int a[], int n) {
+    for (int i = 0; i < n - 1; ++i) {
+        for (int j = i + 1; j < n; ++j) {
+            if (a[i] > a[j]) {
+                Hoanvi(&a[i], &a[j]);
+            }
+        }
+    }
+    for (int i = 0; i < n; ++i) {
+        printf("%d ", a[i]);
+    }
+
+}
+
+void InvaXuatmangtangdan() {
+    int artNumber[] = {8, 2, 3, 1, 5, 7, 9};
+    printf("mang ban dau : \n");
+    xuat(artNumber, 7);
+    printf("Mang a sau khi sap xep tang dan : \n");
+    sapxeptangdan(artNumber, 7);
+
+
+}
+
+int main(){
+    int a=10 ;
+    int *point; // Khai bao bien con tro ten la point va co the tro den cac bien so nguyen (integer)(int)
+    point = &a;// gan dia chi bien a cho con tro
+    printf("Gia tri cua a la : %d\n",a); // in ra gia tri cua bien a
+    printf("Dia chi cua a la : %d\n",&a);//in dia chi bien a
+    printf("Gia tri cua con tro la: %d\n",*point); // in gia tri cua con tro
+    printf("Dia chi cua con tro la: %d\n",point);//in dia chi cua con tro
+
+
+}
